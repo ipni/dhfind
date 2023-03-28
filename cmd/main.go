46722287c94b80f0c135e4c1d8822d10987da749
Ivan Schasny <ivan.schasny@protocol.ai>
2023-03-28 18:22:13 +0100

    Add configurable simulatioin parameters
    * Add configurable simulation channel size to be able to accomodate cid.contact throughout
    * Add configurable simulation worker count so that it can be changed without modifying the service

--- cmd/main.go
+++ cmd/main.go
@@ -21,6 +21,10 @@ func main() {
 	stiAddr := flag.String("stiAddr", "", "The storetheindex HTTP address, used for fetching providers.")
 	metricsAddr := flag.String("metricsAddr", "0.0.0.0:40082", "Prometheus metrics HTTP address.")
 	simulation := flag.Bool("simulation", false, "Whether dhfind runs in simulation mode.")
+	simulationWorkerCount := flag.Int("simulationWorkerCount", 50, "A number of background workers that find tasks are delegated to in simulation mode.")
+	simulationChannelSize := flag.Int("simulationChannelSize", 1000, "A size of the bounded channel that is used by background workers in simulation mode. "+
+		"That number should be high enough to accomodate the throughput of the server.")
+
 	llvl := flag.String("logLevel", "info", "The logging level. Only applied if GOLOG_LOG_LEVEL environment variable is unset.")
 
 	flag.Parse()
@@ -40,7 +44,7 @@ func main() {
 
 	ctx := context.Background()
 
-	server, err := server.New(*listenAddr, *dhstoreAddr, *stiAddr, m, *simulation)
+	server, err := server.New(*listenAddr, *dhstoreAddr, *stiAddr, m, *simulation, *simulationWorkerCount, *simulationChannelSize)
 	if err != nil {
 		panic(err)
 	}
