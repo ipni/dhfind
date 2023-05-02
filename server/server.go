af90d716a49ee0b087ea916901d7e774bac30abe
Ivan Schasny <ivan.schasny@protocol.ai>
2023-05-02 10:12:09 +0100

    Update to threadsafe version of dhclient

--- server/server.go
+++ server/server.go
@@ -32,6 +32,7 @@ var (
 )
 
 type Server struct {
+	c       *finderhttpclient.DHashClient
 	s       *http.Server
 	m       *metrics.Metrics
 	dhaddr  string
@@ -109,6 +110,14 @@ func New(addr, dhaddr, stiaddr string, m *metrics.Metrics, simulation bool, simu
 	server.dhaddr = dhaddr
 	server.stiaddr = stiaddr
 	server.simulation = simulation
+
+	c, err := finderhttpclient.NewDHashClient(dhaddr, stiaddr)
+	if err != nil {
+		return nil, err
+	}
+
+	server.c = c
+
 	if simulation {
 		server.simulationJobs = make(chan simulationJob, simulationChannelSize)
 		server.simulationWorkerCount = simulationWorkerCount
@@ -216,16 +225,10 @@ func (s *Server) handleGetMh(w lookupResponseWriter, r *http.Request) {
 	log := logger.With("multihash", mh)
 	ctx := context.Background()
 
-	c, err := finderhttpclient.NewDHashClient(s.dhaddr, s.stiaddr)
-	if err != nil {
-		s.handleError(w, err, log)
-		return
-	}
-
 	resChan := make(chan model.ProviderResult)
 	errChan := make(chan error)
 
-	go c.FindAsync(ctx, mh, resChan, errChan)
+	go s.c.FindAsync(ctx, mh, resChan, errChan)
 
 	haveResults := false
 	for {
